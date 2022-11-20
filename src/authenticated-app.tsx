8ff840c4b13f6ab3092ff2a7d1ca93a21dc9daf5
Zzrk <1252124617@qq.com>
2022-11-21 02:51:00 +0800

    feat: 用 flex 和 grid 布局页面

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -1,18 +1,7 @@
-import { useAuth } from "context/auth-context";
 import React from "react";
 import { ProjectListScreen } from "screens/project-list";
-// import { ProjectListScreen } from "screens/project-list";
-// import { useAuth } from "context/auth-context";
-// import { ReactComponent as SoftwareLogo } from "assets/software-logo.svg";
-// import styled from "@emotion/styled";
-// import { ButtonNoPadding, Row } from "components/lib";
-// import { Button, Dropdown, Menu } from "antd";
-// import { Navigate, Route, Routes } from "react-router";
-// import { ProjectScreen } from "screens/project";
-// import { resetRoute } from "utils";
-// import { ProjectModal } from "screens/project-list/project-modal";
-// import { ProjectPopover } from "components/project-popover";
-// import { UserPopover } from "components/user-popover";
+import { useAuth } from "context/auth-context";
+import styled from "@emotion/styled";
 
 /**
  * grid 和 flex 各自的应用场景
@@ -26,85 +15,64 @@ import { ProjectListScreen } from "screens/project-list";
  *
  */
 
-// prop drilling
-
 export const AuthenticatedApp = () => {
   const { logout } = useAuth();
-  // return (
-  //   <Container>
-  //     <PageHeader />
-  //     <Main>
-  //       <Routes>
-  //         <Route path={"/projects"} element={<ProjectListScreen />} />
-  //         <Route path={"/projects/:projectId/*"} element={<ProjectScreen />} />
-  //         <Navigate to={"/projects"} />
-  //       </Routes>
-  //     </Main>
-  //     <ProjectModal />
-  //   </Container>
-  // );
   return (
-    <div>
-      <button onClick={logout}>登出</button>
-      <ProjectListScreen />
-    </div>
+    <Container>
+      <Header>
+        <HeaderLeft>
+          <h3>Logo</h3>
+          <h3>项目</h3>
+          <h3>用户</h3>
+        </HeaderLeft>
+        <HeaderRight>
+          <button onClick={logout}>登出</button>
+        </HeaderRight>
+      </Header>
+      <Nav>nav</Nav>
+      <Main>
+        <ProjectListScreen />
+      </Main>
+      <Aside>aside</Aside>
+      <Footer>footer</Footer>
+    </Container>
   );
 };
 
-// const PageHeader = () => {
-//   return (
-//     <Header between={true}>
-//       <HeaderLeft gap={true}>
-//         <ButtonNoPadding type={"link"} onClick={resetRoute}>
-//           <SoftwareLogo width={"18rem"} color={"rgb(38, 132, 255)"} />
-//         </ButtonNoPadding>
-//         <ProjectPopover />
-//         <UserPopover />
-//       </HeaderLeft>
-//       <HeaderRight>
-//         <User />
-//       </HeaderRight>
-//     </Header>
-//   );
-// };
+const Container = styled.div`
+  display: grid;
+  grid-template-rows: 6rem 1fr 6rem;
+  grid-template-columns: 20rem 1fr 20rem;
+  grid-template-areas:
+    "header header header"
+    "nav main aside"
+    "footer footer footer";
+  height: 100vh;
+  grid-gap: 10rem;
+`;
 
-// const User = () => {
-//   const { logout, user } = useAuth();
-//   return (
-//     <Dropdown
-//       overlay={
-//         <Menu>
-//           <Menu.Item key={"logout"}>
-//             <Button onClick={logout} type={"link"}>
-//               登出
-//             </Button>
-//           </Menu.Item>
-//         </Menu>
-//       }
-//     >
-//       <Button type={"link"} onClick={(e) => e.preventDefault()}>
-//         Hi, {user?.name}
-//       </Button>
-//     </Dropdown>
-//   );
-// };
-
-// // temporal dead zone(暂时性死区)
-// const Container = styled.div`
-//   display: grid;
-//   grid-template-rows: 6rem 1fr;
-//   height: 100vh;
-// `;
-
-// // grid-area 用来给grid子元素起名字
-// const Header = styled(Row)`
-//   padding: 3.2rem;
-//   box-shadow: 0 0 5px 0 rgba(0, 0, 0, 0.1);
-//   z-index: 1;
-// `;
-// const HeaderLeft = styled(Row)``;
-// const HeaderRight = styled.div``;
-// const Main = styled.main`
-//   display: flex;
-//   overflow: hidden;
-// `;
+// grid-area 用来给grid子元素起名字
+const Header = styled.header`
+  grid-area: header;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+const HeaderLeft = styled.div`
+  display: flex;
+  align-items: center;
+`;
+const HeaderRight = styled.div``;
+const Main = styled.main`
+  grid-area: main;
+`;
+const Nav = styled.nav`
+  grid-area: nav;
+`;
+const Aside = styled.aside`
+  grid-area: aside;
+`;
+const Footer = styled.footer`
+  grid-area: footer;
+`;
